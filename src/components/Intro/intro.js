111fd4de053f89dde50f7941bd14f590800cd978
piotrzalejski <piotrzalejski@outlook.com>
2023-11-18 19:31:19 -0600

    first commit

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./intro.css";
+import { Link } from "react-scroll"
+
+const Intro = () => {
+    return(
+        <section id="intro">
+            <div className="introContent">
+                <span className="hello">Hello, <br/></span>
+                <span className="introText">I'm <span className="introName">Piotr <br /></span>a Software Developer</span>
+                <p className="introPara">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id, quibusdam veniam! <br />Laudantium neque ipsum porro excepturi, dicta quod aperiam et explicabo?</p>
+                <Link><button className="btn">Hire Me</button></Link>
+            </div>
+            <img src="https://placehold.co/400x600" alt="" className="bg"/>
+        </section>
+    )
+}
+
+export default Intro;
---
+++ src/components/NavBar/navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./navbar.css"
+import logo from "../../assets/logo2.png"
+import {Link} from 'react-scroll'
+
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <img src={logo} width={75} height={75} alt="Logo" className="logo" />
+            <div className="desktopMenu">
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+            </div>
+            <button className="desktopMenuBtn">
+                Contact Me
+            </button>
+        </nav>
+    )
+}
+
+export default Navbar
